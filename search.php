b7ab38eb6e3e9f5a70e389c2353f99d3dbd7726a
SashaHavryliuk <sasg96984@gmail.com>
2024-12-16 19:19:06 +0200

    Add files via upload

---
+++ search.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+	<script src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    
+    <!--[if (gt IE 9)|!(IE)]><!-->
+        <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+    
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header id="header">
+    <div class="container">
+        <div class="headtop">
+            <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="balco" src="img/logo.png"></a></h1> 
+            <a class="bmember" href=""><img src="img/page1_icon1.png" alt="">become a member</a>       
+        </div>
+    </div>
+    <div class="container">
+          <div class="menuheader">
+                <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                    <ul class="nav sf-menu">
+                      <li><a href="index.html">Home</a></li>
+                      <li><a href="index-1.html">About us</a>
+                        <ul>
+                          <li><a href="#">Dolore ipsu</a></li>
+                          <li><a href="#">Consecte</a></li>
+                          <li><a class="last" href="#">Elit Conseq</a>
+                            <ul>
+                               <li><img src="img/arrowsubmenu1.png" alt=""><a href="#">Dolore ipsu</a></li>
+                               <li><a href="#">Consecte</a></li> 
+                               <li><a class="last" href="#">Elit Conseq</a></li>                       
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a href="index-2.html">gallery</a></li>
+                      <li><a href="index-3.html">Articles</a></li>
+                      <li><a href="index-4.html">Contacts</a></li>
+                    </ul>
+                </nav>
+          </div>
+    </div>
+</header>
+<!--==============================content=================================-->
+<section id="content">
+    <!--==============================row_10=================================-->
+    <div class="row_10">
+        <div class="container">
+            <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12">
+               <h2 class="h2-pad">Search result:</h2>
+                <div id="search-results"></div>
+               </div>
+            </div>
+        </div>
+  </div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <div class="footerrow">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="pull-right">
+                        <ul class="social_icons clearfix">
+                             <li><a href="#"><img src="img/follow_icon1.png" alt=""></a></li>
+                             <li><a href="#"><img src="img/follow_icon2.png" alt=""></a></li>
+                             <li><a href="#"><img src="img/follow_icon3.png" alt=""></a></li>
+                             <li><a href="#"><img src="img/follow_icon4.png" alt=""></a></li>
+                        </ul>
+                    </div>
+                    <p class="footerpriv"><strong>BALCO</strong> &copy; 2013 <img src="img/imgline2.jpg" alt=""> <a class="privacylink" href="index-5.html">Privacy Policy</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
